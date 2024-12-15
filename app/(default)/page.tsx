24ce473a3d53133bb734e22f0f7e391ee67e0320
Surya Bumantara <bumantara.surya2213@gmail.com>
2024-12-15 12:54:26 +0700

    update landing page

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,63 +1,45 @@
 "use client"
 
-import { Code, Link, Snippet } from "@nextui-org/react"
+import { Button, Code, Link, Snippet } from "@nextui-org/react"
 import { button } from "@nextui-org/theme"
 import { motion } from "motion/react"
 import { Metadata } from "next"
 
 import { GithubIcon } from "@/components/icons"
-import { subtitle, title } from "@/components/primitives"
+import MyIcon from "@/components/myIcon"
+import { btn, subtitle, title } from "@/components/primitives"
 import { siteConfig } from "@/config/site"
 
 export default function Page() {
   return (
     <motion.div
       animate={{ opacity: 1, y: 0 }}
+      className="flex h-full flex-col items-center justify-center gap-4"
       exit={{ opacity: 0, y: 50 }}
       initial={{ opacity: 0, y: 50 }}
       transition={{ duration: 0.4 }}
     >
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-16">
-        <div className="inline-block max-w-xl text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "green" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
+      <section className="flex flex-col items-center justify-center gap-4 ">
+        <div className="max-w-xl text-center justify-center">
+          <div className={title({ color: "green" })}>Expla&nbsp;</div>
+          <div className="text-4xl font-semibold tracking-tight mt-6 mb-10">
+            Your everyday expense planner
           </div>
         </div>
 
         <div className="flex gap-3">
-          <Link
-            isExternal
-            className={button({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
+          <Button
+            as={Link}
+            className={btn({ fontSize: "lg", className: "rounded-2xl" })}
+            href="/register"
+            size="lg"
+            startContent={
+              <MyIcon color="primary-900" icon="solar:login-2-bold-duotone" />
+            }
+            variant="shadow"
           >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={button({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing <Code color="primary">app/page.tsx</Code>
-            </span>
-          </Snippet>
+            Get Started
+          </Button>
         </div>
       </section>
     </motion.div>
--- components/primitives.ts
+++ components/primitives.ts
@@ -15,7 +15,7 @@ export const title = tv({
     size: {
       xs: "text-md",
       sm: "text-3xl lg:text-4xl",
-      md: "text-[2.3rem] lg:text-5xl leading-9",
+      md: "text-5xl leading-9",
       lg: "text-4xl lg:text-6xl",
     },
     fullWidth: {
